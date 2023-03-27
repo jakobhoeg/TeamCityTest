1e1325f3233f8790dfe50034bb962ffae57b5bf1
Jakob Hoeg Mørk <jakmoe01@easv365.dk>
2023-03-27 08:43:44 +0200

    TEst

---
+++ src/main/test/TestIt.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestIt {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void testNothing()
+    {
+        assertEquals(2,1,2);
+    }
+
+}
