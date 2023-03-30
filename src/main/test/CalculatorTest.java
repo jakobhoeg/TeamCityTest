7331f12d0a0d399e9f7c99f84f7c57294162fd74
Jakob Hoeg Mørk <jakmoe01@easv365.dk>
2023-03-30 12:31:27 +0200

    Testt

--- src/main/test/CalculatorTest.java
+++ src/main/test/CalculatorTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestIt {
+public class CalculatorTest {
+
+    public void calculatorTest() {
+
+    }
 
     @BeforeEach
     void setUp() {
